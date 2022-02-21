f7f694f669d45a6fde63cc75b5028c54ab079c90
oshinoooo <1036858975@qq.com>
2022-02-21 09:35:46 -0500

    2022-02-21 09:35:46

--- problems0083/main.cpp
+++ problems0083/main.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-struct ListNode
-{
+struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
@@ -12,38 +11,48 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution
-{
+class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head)
-    {
-        if (head == nullptr) return nullptr;
-        ListNode* pre = head;
-        while (pre->next != nullptr)
-        {
-            if (pre->val == pre->next->val)
-                pre->next = pre->next->next;
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode* curr = head->next;
+
+        ListNode* newHead = new ListNode(0, head);
+        ListNode* tail = head;
+        tail->next = nullptr;
+
+        while (curr) {
+            if (tail->val != curr->val) {
+                tail->next = curr;
+                curr = curr->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            }
             else
-                pre = pre->next;
+                curr = curr->next;
         }
-        return head;
+
+        return newHead->next;
     }
 };
 
 int main()
 {
     cout << "------------------" << endl;
-    Solution s;
     ListNode* n1 = new ListNode(1);
     ListNode* n2 = new ListNode(2);
     ListNode* n3 = new ListNode(2);
     ListNode* n4 = new ListNode(4);
     ListNode* n5 = new ListNode(4);
+
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
     n4->next = n5;
 
+    Solution s;
     ListNode* out = s.deleteDuplicates(n1);
     while (out != nullptr)
     {
--- problems0322/main.cpp
+++ problems0322/main.cpp
@@ -15,21 +15,28 @@ using namespace std;
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(), coins.end(), greater<int>());
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
 
-        int count = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.size(); ++j) {
+                if (0 <= i - coins[j])
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
 
+        if (dp.back() == amount + 1)
+            return -1;
 
-
-        return count;
+        return dp.back();
     }
 };
 
 int main() {
     cout << "---------------------" << endl;
     Solution s;
-    vector<int> coins = {1, 2, 5};
-    int amount = 11;
+    vector<int> coins = {2};
+    int amount = 3;
     cout << s.coinChange(coins, amount) << endl;
     cout << "---------------------" << endl;
     return 0;
