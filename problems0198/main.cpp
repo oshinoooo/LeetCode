748615c1bcaeab80b5cd69d116fd4421fdbd94b5
oshinoooo <1036858975@qq.com>
2022-02-22 16:49:33 -0500

    2022-02-22 16:49:33

--- problems0198/main.cpp
+++ problems0198/main.cpp
@@ -4,9 +4,17 @@
 
 using namespace std;
 
+struct node {
+    int steal;
+    int noSteal;
+    vector<int> stealPath;
+    vector<int> noStealPath;
+    node() : steal(0), noSteal(0) {}
+};
+
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int rob1(vector<int>& nums) {
         vector<vector<int>> dp(nums.size(), vector<int>(2, 0));
         dp[0][0] = nums[0];
 
@@ -16,6 +24,38 @@ public:
         }
 
         return max(dp.back()[0], dp.back()[1]);
+    }
+
+    int rob(vector<int>& nums) {
+        vector<node> dp(nums.size());
+        dp[0].steal = nums[0];
+        dp[0].stealPath = {0};
+
+        for (int i = 1; i < nums.size(); ++i) {
+            node tmp = dp[i - 1];
+
+            dp[i].steal = nums[i] + tmp.noSteal;
+            dp[i].stealPath = tmp.noStealPath;
+            dp[i].stealPath.push_back(i);
+
+            dp[i].noSteal = max(tmp.steal, tmp.noSteal);
+            if (tmp.steal < tmp.noSteal)
+                dp[i].noStealPath = tmp.noStealPath;
+            else
+                dp[i].noStealPath = tmp.stealPath;
+        }
+
+        vector<int> tmp;
+        if (dp.back().steal < dp.back().noSteal)
+            tmp = dp.back().noStealPath;
+        else
+            tmp = dp.back().stealPath;
+
+        for (int num : tmp)
+            cout << num <<" ";
+        cout << endl;
+
+        return max(dp.back().steal, dp.back().noSteal);
     }
 };
 
