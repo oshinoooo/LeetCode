acedd5f1480390e962b1f6173669ce0f6b53210f
oshinoooo <1036858975@qq.com>
2022-02-22 15:24:27 -0500

    2022-02-22 15:24:27

--- problems0468/main.cpp
+++ problems0468/main.cpp
@@ -15,50 +15,94 @@ using namespace std;
 class Solution {
 public:
     string validIPAddress(string queryIP) {
-        vector<string> nums;
-        string tmp;
+        if (!isalnum(queryIP[0]) || !isalnum(queryIP.back()))
+            return "Neither";
+
+        bool ipv4;
         for (char c : queryIP) {
-            if (isalnum(c))
-                tmp.push_back(c);
-            else {
-                nums.push_back(tmp);
-                tmp.clear();
-                if (c != '.' && c != ':')
+            if (!isalnum(c)) {
+                if (c == '.')
+                    ipv4 = true;
+                else if (c == ':')
+                    ipv4 = false;
+                else
                     return "Neither";
+                break;
             }
         }
 
-        if (isIPv4(nums))
-            return "IPv4";
+        vector<string> nums = separate(queryIP);
 
-        if (isIPv6(nums))
+        if (ipv4) {
+            if (nums.size() != 4)
+                return "Neither";
+
+            for (string num : nums) {
+                if (num.empty())
+                    return "Neither";
+
+                if (3 < num.size())
+                    return "Neither";
+
+                if (1 < num.size() && num[0] == '0')
+                    return "Neither";
+
+                for (char c : num) {
+                    if (c < '0' || '9' < c)
+                        return "Neither";
+                }
+
+                if (255 < stoi(num))
+                    return "Neither";
+            }
+
+            return "IPv4";
+        }
+        else {
+            if (nums.size() != 8)
+                return "Neither";
+
+            for (string num : nums) {
+                if (num.empty())
+                    return "Neither";
+
+                if (4 < num.size())
+                    return "Neither";
+
+                for (char c : num) {
+                    if (isalpha(c) && !(('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')))
+                        return "Neither";
+                }
+            }
             return "IPv6";
+        }
 
         return "Neither";
     }
 
 private:
-    bool isIPv4(const vector<string>& nums) {
-        for (string num : nums) {
-            if (num == "0" || stoi(num) < 0 || 255 < stoi(num))
-                return false;
-        }
-        return true;
-    }
+    vector<string> separate(const string& queryIP) {
+        vector<string> out;
 
-    bool isIPv6(const vector<string>& nums) {
-        for (string num : nums) {
-            if (num.size() < 1 || 4 < num.size())
-                return false;
+        string tmp;
+        for (char c : queryIP) {
+            if (isalnum(c))
+                tmp.push_back(c);
+            else {
+                out.push_back(tmp);
+                tmp.clear();
+            }
         }
-        return true;
+        out.push_back(tmp);
+
+        return out;
     }
 };
 
 int main() {
     cout << "--------------------" << endl;
     Solution s;
-    cout << s.validIPAddress("172.16.254.1") << endl;
+    cout << s.validIPAddress("2001:db8:85a3:0::8a2E:0370:7334") << endl;
     cout << "--------------------" << endl;
     return 0;
 }
