a20d9cc757d55af7e87ddd5b7bbc9521255aa732
oshinoooo <1036858975@qq.com>
2022-02-22 23:06:15 -0500

    2022-02-22 23:06:15

--- Test_Practise/main.cpp
+++ Test_Practise/main.cpp
@@ -1,27 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <string.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-mutex m;
-
-void show(int i) {
-    lock_guard<mutex> lock(m);
-    for (int j = 0; j < 50; ++j)
-        cout << i << " ";
-    cout << endl;
-}
-
 int main() {
     cout << "----------------------------------------" << endl;
-    thread t1(show, 1);
-    thread t2(show, 2);
-    t1.join();
-    t2.join();
+    int a[5] = {1, 2, 3, 4, 5};
+    int* ptr = (int*)(&a + 1);
+
+    cout << a << endl;
+    cout << *a << endl;
+    cout << *(a + 1) << endl;
+    cout << &a << endl;
+    cout << &a + 1 << endl;
+
+    printf("%d, %d\n", *(a + 1), *(ptr - 1));
     cout << "----------------------------------------" << endl;
     return 0;
 }
