28219f400a829047d93141c2d53d5d59e0a119b6
oshinoooo <1036858975@qq.com>
2022-02-24 19:29:31 -0500

    2022-02-24 19:29:31

--- problems0091/main.cpp
+++ problems0091/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <algorithm>
+#include <functional>
 #include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
-    int numDecoding(string s) {
+    int numDecodings(string s) {
         int n = s.size();
         vector<int> dp(n + 1);
         dp[0] = 1;
@@ -22,21 +31,12 @@ public:
 
         return dp[n];
     }
-
-    int strStr(string haystack, string needle) {
-        for (int i = 0; i <= haystack.size() - needle.size(); ++i) {
-            if (haystack.substr(i, needle.size()) == needle)
-                return i;
-        }
-        return -1;
-    }
 };
 
 int main() {
     cout << "--------------------" << endl;
     Solution s;
-    cout << s.numDecoding("11106") << endl;
-    cout << s.strStr("shein", "in") << endl;
+    cout << s.numDecodings("11106") << endl;
     cout << "--------------------" << endl;
     return 0;
 }
