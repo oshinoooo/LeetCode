d2f892528777ad2014618e128e0268e600b8d838
oshinoooo <1036858975@qq.com>
2022-02-26 21:32:26 -0500

    2022-02-26 21:32:26

--- Test_Practise/main.cpp
+++ Test_Practise/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class TestPrint{
+class TestPrint {
 public:
     TestPrint() : ptr(0), str("12345"), printCount(100) {}
 
@@ -31,7 +31,61 @@ private:
     condition_variable cv;
 };
 
-int main() {
+class Solution {
+public:
+    int count(const vector<int>& nums) {
+        int count = 1;
+        int ptr1 = 0;
+        int ptr2 = nums.size() - 1;
+        int prev = nums[ptr1] < nums[ptr2] ? ptr1++ : ptr2--;
+
+        while (ptr1 <= ptr2) {
+            int tmp = nums[ptr1] < nums[ptr2] ? ptr1++ : ptr2--;
+            if (nums[tmp] != nums[prev]) {
+                ++count;
+                prev = tmp;
+            }
+        }
+
+        return count;
+    }
+
+    int count2(vector<int>& nums) {
+        quick_sort(nums, 0, nums.size() - 1);
+        int count = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i - 1] != nums[i])
+                ++count;
+        }
+        return count;
+    }
+
+private:
+    void quick_sort(vector<int>& nums, int head, int tail) {
+        if (tail <= head)
+            return;
+
+        int pivot = nums[head];
+        int tmpHead = head;
+        int tmpTail = tail;
+
+        while (tmpHead < tmpTail) {
+            while (tmpHead < tmpTail && pivot <= nums[tmpTail])
+                --tmpTail;
+            nums[tmpHead] = nums[tmpTail];
+
+            while (tmpHead < tmpTail && nums[tmpHead] <= pivot)
+                ++tmpHead;
+            nums[tmpTail] = nums[tmpHead];
+        }
+
+        nums[tmpHead] = pivot;
+        quick_sort(nums, head, tmpHead - 1);
+        quick_sort(nums, tmpHead + 1, tail);
+    }
+};
+
+int test1() {
     cout << "----------------------------------------" << endl;
     TestPrint testPrint;
 
@@ -41,6 +95,23 @@ int main() {
 
     for (int i = 0; i < threads.size(); ++i)
         threads[i].join();
+    cout << "----------------------------------------" << endl;
+    return 0;
+}
+
+int test2() {
+    cout << "----------------------------------------" << endl;
+    vector<int> nums = {1, 1, 3, 9, 10, 30, 11, 10, 2};
+    Solution s;
+    cout << s.count(nums) << endl;
+    cout << "----------------------------------------" << endl;
+    return 0;
+}
+
+int main() {
+    cout << "----------------------------------------" << endl;
+//    test1();
+    test2();
     cout << "----------------------------------------" << endl;
     return 0;
 }
