67744b3e791c990d883a09b99612e6c68970ef85
oshinoooo <1036858975@qq.com>
2022-02-26 22:08:14 -0500

    2022-02-26 22:08:14

--- problems0053/main.cpp
+++ problems0053/main.cpp
@@ -5,45 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubArray1(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        int max = INT_MIN;
-        for (int i = 0; i < nums.size(); ++i) {
-            int sum = nums[i];
-            if (sum > max)
-                max = sum;
-            for (int j = i + 1; j < nums.size(); ++j) {
-                sum += nums[j];
-                if (sum > max)
-                    max = sum;
-            }
-        }
-        return max;
-    }
-
-    int maxSubArray2(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        int out = nums[0];
-        vector<int> sum = {nums[0]};
-
-        for (int i = 1; i < nums.size(); ++i) {
-            int tmp = max(sum[i - 1] + nums[i], nums[i]);
-            sum.push_back(tmp);
-            if (out < tmp)
-                out = tmp;
-        }
-        return out;
-    }
-
     int maxSubArray(vector<int>& nums) {
-
+        int preMin = 0;
+        int preSum = 0;
+        int maxSum = INT_MIN;
+        for (int i = 0; i < nums.size(); ++i) {
+            preSum += nums[i];
+            maxSum = max(maxSum, preSum - preMin);
+            preMin = min(preMin, preSum);
+        }
+        return maxSum;
     }
 };
 
 int main() {
     cout << "--------------------" << endl;
     Solution s;
-    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> nums = {-1};
     cout << s.maxSubArray(nums) << endl;
     cout << "--------------------" << endl;
     return 0;
