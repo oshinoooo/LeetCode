d5e6775175fae9c05afc5d9e6224a0dae96b30f4
oshinoooo <1036858975@qq.com>
2022-02-28 14:24:02 -0500

    2022-02-28 14:24:02

--- problems0807/main.cpp
+++ problems0807/main.cpp
@@ -15,14 +15,37 @@ using namespace std;
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
 
+        vector<int> west(m, 0);
+        vector<int> north(n, 0);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                west[i] = max(west[i], grid[i][j]);
+                north[j] = max(north[j], grid[i][j]);
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j)
+                sum += min(west[i], north[j]) - grid[i][j];
+        }
+
+        return sum;
     }
 };
 
 int main() {
     cout << "--------------------" << endl;
     Solution s;
-    vector<vector<int>> grid = {{}};
+    vector<vector<int>> grid = {{3, 0, 8, 4},
+                                {2, 4, 5, 7},
+                                {9, 2, 6, 3},
+                                {0, 3, 1, 0}};
     cout << s.maxIncreaseKeepingSkyline(grid) << endl;
     cout << "--------------------" << endl;
     return 0;
