57de31ec6447bb37f1585cdd2fa1ca0135f0f9c8
oshinoooo <1036858975@qq.com>
2022-02-27 21:36:23 -0500

    2022-02-27 21:36:23

--- problems0162/main.cpp
+++ problems0162/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int length = nums.size();
+
+        if (length == 1)
+            return 0;
+
+        int head = 0;
+        int tail = length - 1;
+
+        while (head <= tail) {
+            int mid = (head + tail) / 2;
+
+            if (mid + 1 < nums.size() && nums[mid] < nums[mid + 1])
+                head = mid + 1;
+            else
+                tail = mid - 1;
+        }
+
+        return head;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "--------------------" << endl;
+    Solution s;
+    vector<int> nums = {0, 1};
+    int i = s.findPeakElement(nums);
+    cout << i << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
