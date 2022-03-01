d8c7023b3d36031a7c405a364d5f97c9aee8223c
oshinoooo <1036858975@qq.com>
2022-02-28 22:05:52 -0500

    2022-02-28 22:05:52

--- problems0040/main.cpp
+++ problems0040/main.cpp
@@ -18,15 +18,17 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> out;
 
+        return out;
     }
 };
 
 int main() {
     cout << "---------------------" << endl;
     Solution s;
-    vector<int> candidates = {1, 2, 3, 4};
-    int target = 4;
+    vector<int> candidates = {10,1,2,7,6,1,5};
+    int target = 8;
     vector<vector<int>> out = s.combinationSum2(candidates, target);
     for (auto ou : out) {
         for (auto o : ou)
