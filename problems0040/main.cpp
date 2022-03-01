cc54300a26cc6efd89a4e7baf1268f16b86fc148
oshinoooo <1036858975@qq.com>
2022-03-01 10:43:56 -0500

    2022-03-01 10:43:56

--- problems0040/main.cpp
+++ problems0040/main.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <deque>
 #include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
-#include <ctime>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> out;
-
+        sort(candidates.begin(), candidates.end());
+        for (int& num : candidates)
+            ++m[num];
+        myCombinationSum2(candidates, target, 0);
         return out;
     }
+
+private:
+    void myCombinationSum2(const vector<int>& candidates, int target, int index) {
+        if (target == 0) {
+            out.push_back(tmp);
+            return;
+        }
+        else if (target < 0 || candidates.size() <= index)
+            return;
+
+        for (int i = 0; i <= m[candidates[index]]; ++i) {
+            for (int j = 0; j < i; ++j)
+                tmp.push_back(candidates[index]);
+
+            myCombinationSum2(candidates, target - candidates[index] * i, index + m[candidates[index]]);
+
+            for (int j = 0; j < i; ++j)
+                tmp.pop_back();
+        }
+    }
+
+private:
+    map<int, int> m;
+    vector<int> tmp;
+    vector<vector<int>> out;
 };
 
 int main() {
     cout << "---------------------" << endl;
     Solution s;
-    vector<int> candidates = {10,1,2,7,6,1,5};
+    vector<int> candidates = {10, 1, 2, 7, 6, 1, 5};
     int target = 8;
     vector<vector<int>> out = s.combinationSum2(candidates, target);
     for (auto ou : out) {
