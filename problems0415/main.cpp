07b10bb45add025c635ed4dad4f60f8ce0962001
oshinoooo <1036858975@qq.com>
2022-03-02 13:00:02 -0500

    2022-03-02 13:00:02

--- problems0415/main.cpp
+++ problems0415/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -35,12 +36,66 @@ public:
 
         return out;
     }
+
+    string addStrings36(string num1, string num2) {
+        int m = num1.size();
+        int n = num2.size();
+
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int carry = 0;
+        string out;
+
+        while (0 <= ptr1 || 0 <= ptr2) {
+            char tmp;
+            if (0 <= ptr1 && 0 <= ptr2)
+                tmp = adder(num1[ptr1--], num2[ptr2--], carry);
+            else if (0 <= ptr1)
+                tmp = adder(num1[ptr1--], '0', carry);
+            else
+                tmp = adder('0', num2[ptr2--], carry);
+
+            out.push_back(tmp);
+        }
+
+        if (carry)
+            out.push_back(index[carry]);
+
+        reverse(out.begin(), out.end());
+
+        return out;
+    }
+
+private:
+     char adder(char a, char b, int& carry) {
+        if (isalpha(a))
+            carry += a - 'a' + 10;
+        else
+            carry += a - '0';
+
+        if (isalpha(b))
+            carry += b - 'a' + 10;
+        else
+            carry += b - '0';
+
+        char out = index[carry % 36];
+
+        carry /= 36;
+
+        return out;
+    }
+
+private:
+    vector<char> index = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+                          'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',};
 };
 
 int main() {
     cout << "---------------------" << endl;
     Solution s;
     cout << s.addStrings("456", "77") << endl;
+    cout << s.addStrings36("az", "az") << endl;
     cout << "---------------------" << endl;
     return 0;
 }
