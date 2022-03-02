8c0a933a9c2e9b8e26b02219b9bbc1e3a3ed6609
oshinoooo <1036858975@qq.com>
2022-03-02 16:46:36 -0500

    2022-03-02 16:46:36

--- offer0013/main.cpp
+++ offer0013/main.cpp
@@ -1,68 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
 #include <queue>
-#include <deque>
-#include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
+#include <vector>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int movingCount(int m, int n, int k)
-    {
-        vector<vector<bool>> board_available(m, vector<bool>(n, false));
-        return my_movingCount(0, 0, k, board_available);
+    int movingCount(int m, int n, int k) {
+        vector<vector<bool>> grid(m, vector<bool>(n, true));
+        queue<pair<int, int>> que;
+        que.push({0, 0});
+        int count = 0;
+
+        while (!que.empty()) {
+            auto [x, y] = que.front();
+            que.pop();
+
+            if (!grid[x][y])
+                continue;
+
+            if (smaller(x, y, k)) {
+                grid[x][y] = false;
+                ++count;
+                if (x + 1 < m)
+                    que.push({x + 1, y});
+                if (y + 1 < n)
+                    que.push({x, y + 1});
+            }
+        }
+
+        return count;
     }
 
 private:
-    int my_movingCount(int row, int col, int& k, vector<vector<bool>>& board_available)
-    {
-        if (row < 0 || col < 0 || board_available.size() <= row || board_available[0].size() <= col)
-            return 0;
+    bool smaller(int x, int y, int k) {
+        int tmp = 0;
 
-        if (board_available[row][col])
-            return 0;
-
-        int sum = 0;
-        int tmp_row = row;
-        while (0 < tmp_row)
-        {
-            sum += tmp_row % 10;
-            tmp_row /= 10;
+        while (x) {
+            tmp += x % 10;
+            x /= 10;
         }
-        int tmp_col = col;
-        while (0 < tmp_col)
-        {
-            sum += tmp_col % 10;
-            tmp_col /= 10;
+
+        while (y) {
+            tmp += y % 10;
+            y /= 10;
         }
-        if (k < sum)
-            return 0;
 
-        board_available[row][col] = true;
-
-        int ret = my_movingCount(row + 1, col, k, board_available) +
-                  my_movingCount(row - 1, col, k, board_available) +
-                  my_movingCount(row, col + 1, k, board_available) +
-                  my_movingCount(row, col - 1, k, board_available) + 1;
-        return ret;
+        return tmp <= k;
     }
 };
 
-int main()
-{
-    cout << "---------------------" << endl;
+int main() {
+    cout << "--------------------" << endl;
     Solution s;
-    cout << s.movingCount(2, 3, 1) << endl;
-    cout << "---------------------" << endl;
+    cout << s.movingCount(3, 2, 17) << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
--- problems0062/main.cpp
+++ problems0062/main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <algorithm>
-#include <functional>
 #include <vector>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
+    int uniquePaths1(int m, int n) {
         vector<vector<int>> matrix(m + 1, vector<int>(n + 1, 0));
         matrix[0][1] = 1;
 
@@ -24,6 +15,17 @@ public:
         }
 
         return matrix.back().back();
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m + 1, vector<int>(n + 1, 0));
+        grid[0][1] = 1;
+        for (int i = 1; i < m + 1; ++i) {
+            for (int j = 1; j < n + 1; ++j) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid.back().back();
     }
 };
 
