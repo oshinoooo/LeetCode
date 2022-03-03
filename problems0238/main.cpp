715e8c5b62284f83215442aadbd0ebc2ff4c7686
oshinoooo <1036858975@qq.com>
2022-03-03 09:54:46 -0500

    2022-03-03 09:54:46

--- problems0238/main.cpp
+++ problems0238/main.cpp
@@ -1,64 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> productExceptSelf1(vector<int>& nums)
-    {
-        vector<int> left = {1};
-        for (int i = 0; i < nums.size() - 1; ++i)
-            left.push_back(left.back() * nums[i]);
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
 
-        vector<int> right = {1};
-        for (int i = nums.size() - 1; 0 < i; --i)
-            right.push_back(right.back() * nums[i]);
+        vector<int> left(n, 1);
+        for (int i = 0; i < n - 1; ++i)
+            left[i + 1] = left[i] * nums[i];
+
+        vector<int> right(n, 1);
+        for (int i = n - 1; 0 < i; --i)
+            right[i - 1] = right[i] * nums[i];
 
         vector<int> out;
-        for (int i = 0; i < left.size(); ++i)
-            out.push_back(left[i] * right[right.size() - i - 1]);
-        return out;
-    }
-
-    vector<int> productExceptSelf(vector<int>& nums)
-    {
-        vector<int> out = {1};
-        for (int i = 0; i < nums.size() - 1; ++i)
-            out.push_back(out.back() * nums[i]);
-
-        int tmp = 1;
-        for (int i = nums.size() - 1; 0 < i; --i)
-        {
-            tmp *= nums[i];
-            out[i - 1] *= tmp;
-        }
+        for (int i = 0; i < n; ++i)
+            out.push_back(left[i] * right[i]);
 
         return out;
     }
 };
 
-int main()
-{
-    cout << "---------------------" << endl;
+int main() {
+    cout << "--------------------" << endl;
     Solution s;
-    vector<int> nums = {1,2,3,4};
+    vector<int> nums = {-1,1,0,-3,3};
     vector<int> out = s.productExceptSelf(nums);
     for (auto num : out)
         cout << num << " ";
     cout << endl;
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
