6fc7d3f360dad2916df1c651ebb5d6e2471895d2
oshinoooo <1036858975@qq.com>
2022-03-04 15:03:28 -0500

    2022-03-04 15:03:28

--- problems0084/main.cpp
+++ problems0084/main.cpp
@@ -6,30 +6,53 @@ using namespace std;
 
 class Solution {
 public:
+    int largestRectangleArea1(vector<int>& heights) {
+        int n = heights.size();
+
+        vector<int> left(n);
+        stack<int> sta;
+        for (int i = 0; i < n; ++i) {
+            while (!sta.empty() && heights[i] <= heights[sta.top()])
+                sta.pop();
+            left[i] = sta.empty() ? -1 : sta.top();
+            sta.push(i);
+        }
+
+        vector<int> right(n);
+        sta = stack<int>();
+        for (int i = n - 1; 0 <= i; --i) {
+            while (!sta.empty() && heights[i] <= heights[sta.top()])
+                sta.pop();
+            right[i] = sta.empty() ? n : sta.top();
+            sta.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; ++i)
+            maxArea = max(maxArea, heights[i] * (right[i] - left[i] - 1));
+
+        return maxArea;
+    }
+
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
 
         vector<int> left(n);
-        stack<int> left_stack;
-        for (int i = 0; i < n; ++i) {
-            while (!left_stack.empty() && heights[i] <= heights[left_stack.top()])
-                left_stack.pop();
+        vector<int> right(n, n);
+        stack<int> sta;
 
-            left[i] = (left_stack.empty() ? -1 : left_stack.top());
-            left_stack.push(i);
+        for (int i = 0; i < n; ++i) {
+            while (!sta.empty() && heights[i] <= heights[sta.top()]) {
+                right[sta.top()] = i;
+                sta.pop();
+            }
+            left[i] = (sta.empty() ? -1 : sta.top());
+            sta.push(i);
         }
 
-        vector<int> right(n);
-        stack<int> right_stack;
-
-
-
-
-
-
         int maxArea = 0;
-
-
+        for (int i = 0; i < n; ++i)
+            maxArea = max(maxArea, heights[i] * (right[i] - left[i] - 1));
 
         return maxArea;
     }
