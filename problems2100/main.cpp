6133e27095024549afa85ac8e9de976d03d6fe1d
oshinoooo <1036858975@qq.com>
2022-03-05 15:53:37 -0500

    2022-03-05 15:53:37

--- problems2100/main.cpp
+++ problems2100/main.cpp
@@ -15,9 +15,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
-        vector<int> out;
+        int n = security.size();
 
-        out = security;
+        vector<int> left(n, 0);
+        for (int i = 1; i < n; ++i) {
+            if (security[i - 1] >= security[i])
+                left[i] = left[i - 1] + 1;
+        }
+
+        vector<int> right(n, 0);
+        for (int i = n - 2; 0 <= i; --i) {
+            if (security[i] <= security[i + 1])
+                right[i] = right[i + 1] + 1;
+        }
+
+        vector<int> out;
+        for (int i = 0; i < n; ++i) {
+            if (time <= left[i] && time <= right[i])
+                out.push_back(i);
+        }
 
         return out;
     }
@@ -26,7 +42,7 @@ public:
 int main() {
     cout << "--------------------" << endl;
     Solution s;
-    vector<int> security = {5,3,3,3,5,6,2};
+    vector<int> security = {5, 3, 3, 3, 5, 6, 2};
     int time = 2;
     vector<int> out = s.goodDaysToRobBank(security, time);
     for (auto num : out)
