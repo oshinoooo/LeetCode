d2f5c2261da090c4a6d1968490e95028fc790dfd
oshinoooo <1036858975@qq.com>
2022-03-05 17:57:40 -0500

    2022-03-05 17:57:40

--- problems0152/main.cpp
+++ problems0152/main.cpp
@@ -1,42 +1,48 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int maxProduct(vector<int>& nums)
-    {
-        vector <int> max_mul = {nums[0]};
-        vector <int> min_mul = {nums[0]};
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            max_mul.push_back(max(max_mul.back() * nums[i], min_mul.back() * nums[i]));
-//            maxF[i] = max(maxF[i - 1] * nums[i], max(nums[i], minF[i - 1] * nums[i]));
-//            minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));
+    int maxProduct1(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxMul = INT_MIN;
+
+        for (int i = 0; i < n; ++i) {
+            int tmpMul = 1;
+            for (int j = i; j < n; ++j) {
+                tmpMul *= nums[j];
+                maxMul = max(maxMul, tmpMul);
+            }
         }
-        return *max_element(maxF.begin(), maxF.end());
+
+        return maxMul;
+    }
+
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxProd = nums[0];
+        vector<int> dpMax(nums);
+        vector<int> dpMin(nums);
+
+        for (int i = 1; i < n; ++i) {
+            dpMax[i] = max(nums[i], max(dpMax[i - 1] * nums[i], dpMin[i - 1] * nums[i]));
+            dpMin[i] = min(nums[i], min(dpMax[i - 1] * nums[i], dpMin[i - 1] * nums[i]));
+            maxProd = max(maxProd, dpMax[i]);
+        }
+
+        return maxProd;
     }
 };
 
-int main()
-{
-    cout << "---------------------" << endl;
+int main() {
+    cout << "--------------------" << endl;
     Solution s;
-    vector<int> nums = {2, 3, -2, 4};
+    vector<int> nums = {2,-1,1,1};
     cout << s.maxProduct(nums) << endl;
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
