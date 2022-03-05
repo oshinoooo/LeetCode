4d3029cae28c40c942209be138ed84d3e371609c
oshinoooo <1036858975@qq.com>
2022-03-04 20:54:51 -0500

    2022-03-04 20:54:51

---
+++ problems0072/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int minDis = INT_MAX;
+
+
+
+
+        return minDis;
+    }
+};
+
+int main() {
+    cout << "--------------------" << endl;
+    Solution s;
+    cout << s.minDistance("horse", "ros") << endl;
+    cout << "--------------------" << endl;
+    return 0;
+}
