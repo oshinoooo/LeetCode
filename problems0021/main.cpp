cac19995b33ff844f2e5045340f9a085e7e82d4a
oshinoooo <1036858975@qq.com>
2022-03-17 16:55:24 -0400

    2022-03-17 16:55:24

--- problems0021/main.cpp
+++ problems0021/main.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <deque>
 #include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
-#include <ctime>
 
 using namespace std;
 
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int _val) : val(_val), next(nullptr) {}
+    ListNode(int _val, ListNode* _next) : val(_val), next(_next) {}
 };
 
 class Solution {
@@ -54,7 +42,7 @@ public:
         return newHead->next;
     }
 
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* mergeTwoLists2(ListNode* list1, ListNode* list2) {
         if (list1 && list2) {
             if (list1->val < list2->val) {
                 list1->next = mergeTwoLists(list1->next, list2);
@@ -69,6 +57,31 @@ public:
             return list1;
 
         return list2;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* newHead = new ListNode();
+        ListNode* curr = newHead;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+
+        if (list1)
+            curr->next = list1;
+        if (list2)
+            curr->next = list2;
+
+        ListNode* out = newHead->next;
+        delete newHead;
+        return out;
     }
 };
 
