4598d936a194ee256606f8b1c5210f7b0e05e6ad
oshinoooo <1036858975@qq.com>
2022-03-20 14:56:32 -0400

    2022-03-20 14:56:32

---
+++ problems0653/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root)
+            return false;
+
+        if (s.count(k - root->val))
+            return true;
+
+        s.insert(root->val);
+
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+
+private:
+    unordered_set<int> s;
+};
+
+int main() {
+    cout << "--------------------" << endl;
+    TreeNode* n1 = new TreeNode(4);
+    TreeNode* n2 = new TreeNode(2);
+    TreeNode* n3 = new TreeNode(6);
+    TreeNode* n4 = new TreeNode(1);
+    TreeNode* n5 = new TreeNode(3);
+    TreeNode* n6 = new TreeNode(5);
+    TreeNode* n7 = new TreeNode(7);
+
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
+
+    Solution s;
+    cout << s.findTarget(n1, 3) << endl;
+    cout << "--------------------" << endl;
+    return 0;
+}
