78d3bffa40f5101b4b9481753c698a088d26924d
oshinoooo <1036858975@qq.com>
2022-03-21 14:59:58 -0400

    2022-03-21 14:59:57

--- problems2038/main.cpp
+++ problems2038/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-        
+        vector<int> freq = {0, 0};
+        char cur = 'C';
+        int cnt = 0;
+        for (const char& c : colors) {
+            if (c != cur) {
+                cur = c;
+                cnt = 1;
+            }
+            else if (3 <= ++cnt) {
+                ++freq[cur - 'A'];
+            }
+        }
+        return freq[0] > freq[1];
     }
 };
 
