df9f387b1368d351db35944cea9795ea48d3c32d
oshinoooo <1036858975@qq.com>
2022-03-25 11:44:37 -0400

    2022-03-25 11:44:37

--- problems0033/main.cpp
+++ problems0033/main.cpp
@@ -3,48 +3,8 @@
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int search1(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == target)
-                return i;
-        }
-        return -1;
-    }
-
-    int search2(vector<int>& nums, int target) {
-        if (nums.size() == 0)
-            return -1;
-
-        if (nums.size() == 1)
-            return nums[0] == target ? 0 : -1;
-
-        int ptr1 = 0;
-        int ptr2 = nums.size() - 1;
-
-        while (ptr1 <= ptr2) {
-            int mid = (ptr1 + ptr2) / 2;
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[0] <= nums[mid]) {
-                if (nums[0] <= target && target < nums[mid])
-                    ptr2 = mid - 1;
-                else
-                    ptr1 = mid + 1;
-            }
-            else {
-                if (nums[mid] < target && target <= nums[nums.size() - 1])
-                    ptr1 = mid + 1;
-                else
-                    ptr2 = mid - 1;
-            }
-        }
-        return -1;
-    }
-
     int search(vector<int>& nums, int target) {
         int head = 0;
         int tail = nums.size() - 1;
@@ -56,13 +16,13 @@ public:
                 return mid;
 
             if (nums[0] <= target) {
-                if (target > nums[mid] && nums[0] <= nums[mid])
+                if (nums[mid] < target && nums[0] <= nums[mid])
                     head = mid + 1;
                 else
                     tail = mid - 1;
             }
             else {
-                if (target < nums[mid] && nums[0] > nums[mid])
+                if (target < nums[mid] && nums[mid] < nums[0])
                     tail = mid - 1;
                 else
                     head = mid + 1;
@@ -75,11 +35,11 @@ public:
 
 int main()
 {
-    cout << "---------------" << endl;
+    cout << "--------------------" << endl;
     Solution s;
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums = {4,5,6,7,0,1,2};
     int target = 0;
     cout << s.search(nums, target) << endl;
-    cout << "---------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
