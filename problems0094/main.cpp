96b67f76d9298043b7a87372a5b8a86f3f57d9a0
oshinoooo <1036858975@qq.com>
2022-03-26 15:43:35 -0400

    2022-03-26 15:43:35

--- problems0094/main.cpp
+++ problems0094/main.cpp
@@ -1,75 +1,81 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
 };
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root)
-    {
-        if (root == nullptr)
-            return {};
+    vector<int> inorderTraversal1(TreeNode* root) {
+        vector<int> out;
+        myInorderTraversal(root, out);
+        return out;
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> stk;
+        while (root) {
+            stk.push(root);
+            root = root->left;
+        }
 
         vector<int> out;
-        vector<int> tmp;
+        while (!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
 
-        tmp = inorderTraversal(root->left);
-        out.insert(out.end(), tmp.begin(), tmp.end());
+            out.push_back(node->val);
 
-        out.push_back(root->val);
-
-        tmp = inorderTraversal(root->right);
-        out.insert(out.end(), tmp.begin(), tmp.end());
+            node = node->right;
+            while (node) {
+                stk.push(node);
+                node = node->left;
+            }
+        }
 
         return out;
     }
 
-    vector<int> inorderTraversal2(TreeNode* root)
-    {
-        vector<int> nums;
-        LDR(root, nums);
-        return nums;
-    }
-
-    void LDR(TreeNode* root, vector<int>& nums)
-    {
-        if (root == nullptr)
-            return;
-        LDR(root->left, nums);
-        nums.push_back(root->val);
-        LDR(root->right, nums);
+private:
+    void myInorderTraversal(TreeNode* root, vector<int>& out) {
+        if (!root) return;
+        myInorderTraversal(root->left, out);
+        out.push_back(root->val);
+        myInorderTraversal(root->right, out);
     }
 };
 
-int main()
-{
-    cout << "-------------------------" << endl;
-    Solution s;
-    TreeNode* n1 = new TreeNode(1);
+int main() {
+    cout << "--------------------" << endl;
+    TreeNode* n1 = new TreeNode(4);
     TreeNode* n2 = new TreeNode(2);
-    TreeNode* n3 = new TreeNode(3);
-    TreeNode* n4 = new TreeNode(4);
-    TreeNode* n5 = new TreeNode(5);
+    TreeNode* n3 = new TreeNode(6);
+    TreeNode* n4 = new TreeNode(1);
+    TreeNode* n5 = new TreeNode(3);
+    TreeNode* n6 = new TreeNode(5);
+    TreeNode* n7 = new TreeNode(7);
+
     n1->left = n2;
     n1->right = n3;
     n2->left = n4;
     n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
 
+    Solution s;
     vector<int> out = s.inorderTraversal(n1);
-
-    for (auto o : out)
-        cout << o << endl;
-    cout << "-------------------------" << endl;
+    for (auto num : out)
+        cout << num << " ";
+    cout << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
--- problems0704/main.cpp
+++ problems0704/main.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int>& nums, int target)
-    {
-        int ptr1 = 0;
-        int ptr2 = nums.size() - 1;
-        while (ptr1 <= ptr2)
-        {
-            int mid = (ptr1 + ptr2) / 2;
+    int search(vector<int>& nums, int target) {
+        int head = 0;
+        int tail = nums.size() - 1;
+
+        while (head <= tail) {
+            int mid = (head + tail) / 2;
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] < target)
-                ptr1 = mid + 1;
+                head = mid + 1;
             else
-                ptr2 = mid - 1;
+                tail = mid - 1;
         }
+
         return -1;
     }
 };
 
-int main()
-{
-    cout << "---------------------" << endl;
+int main() {
+    cout << "--------------------" << endl;
     Solution s;
-    vector<int> nums = {-1,0,3,5,9,12};
+    vector<int> nums = {-1,0,2,5,9,12};
     int target = 2;
     cout << s.search(nums, target) << endl;
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
