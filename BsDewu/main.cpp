baa8681614c1b6f4f2bf0dc403e7e755d3d83af4
oshinoooo <1036858975@qq.com>
2022-03-31 09:05:03 -0400

    2022-03-31 09:05:03

--- BsDewu/main.cpp
+++ BsDewu/main.cpp
@@ -17,30 +17,14 @@ public:
     // 逆转字符串
     // 判断IPv4/IPv6
     // 青蛙跳台阶
+
+    //
+    //
+    //
 };
 
-void test1() {
-    cout << "---------------------" << endl;
-    Solution s;
-    cout << "" << endl;
-}
-
-void test2() {
-    cout << "---------------------" << endl;
-    Solution s;
-    cout << "" << endl;
-}
-
-void test3() {
-    cout << "---------------------" << endl;
-    Solution s;
-    cout << "" << endl;
-}
-
 int main() {
-    test1();
-    test2();
-    test3();
+    cout << "---------------------" << endl;
     cout << "---------------------" << endl;
     return 0;
 }
