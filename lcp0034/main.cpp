c5177d26c5f90fe01d324b5d42271ff24e147af7
oshinoooo <1036858975@qq.com>
2022-04-08 15:16:43 -0400

    2022-04-08 15:16:43

---
+++ lcp0034/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
+};
+
+class Solution {
+public:
+    int maxValue(TreeNode* root, int k) {
+
+    }
+};
+
+int main() {
+    cout << "--------------------" << endl;
+    TreeNode* n1 = new TreeNode(4);
+    TreeNode* n2 = new TreeNode(2);
+    TreeNode* n3 = new TreeNode(6);
+    TreeNode* n4 = new TreeNode(1);
+    TreeNode* n5 = new TreeNode(3);
+    TreeNode* n6 = new TreeNode(5);
+    TreeNode* n7 = new TreeNode(7);
+
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
+
+    Solution s;
+    cout << s.maxValue(n1, 2) << endl;
+    cout << "--------------------" << endl;
+    return 0;
+}
