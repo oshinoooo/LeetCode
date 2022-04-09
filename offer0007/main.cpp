ccab40859b896edd11869733fec8b7809f072075
oshinoooo <1036858975@qq.com>
2022-04-09 16:21:19 -0400

    2022-04-09 16:21:19

--- offer0007/main.cpp
+++ offer0007/main.cpp
@@ -1,99 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
 #include <unordered_map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <cctype>
-#include <numeric>
-#include <math.h>
 
 using namespace std;
 
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
 };
+
+void showTree(TreeNode* root) {
+    if (!root) return;
+    showTree(root->left);
+    cout << root->val << " ";
+    showTree(root->right);
+}
 
 class Solution {
 public:
-//    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-//        return my_buildTree(preorder, inorder);
-//    }
-//
-//private:
-//    TreeNode* my_buildTree(vector<int>& preorder, vector<int>& inorder) {
-//        if (preorder.empty()) {
-//            return nullptr;
-//        }
-//
-//        TreeNode* root = new TreeNode(preorder[0]);
-//
-//        vector<int> left_preorder;
-//        vector<int> left_inorder;
-//        vector<int> right_preorder;
-//        vector<int> right_inorder;
-//
-//        for (int i = 0; i < inorder.size(); ++i) {
-//            if (inorder[i] == preorder[0]) {
-//                left_preorder.insert(left_preorder.begin(), preorder.begin() + 1, preorder.begin() + 1 + i);
-//                left_inorder.insert(left_inorder.begin(), inorder.begin(), inorder.begin() + i);
-//                right_preorder.insert(right_preorder.begin(), preorder.begin() + 1 + i, preorder.end());
-//                right_inorder.insert(right_inorder.begin(), inorder.begin() + i + 1, inorder.end());
-//            }
-//        }
-//
-//        root->left = my_buildTree(left_preorder, left_inorder);
-//        root->right = my_buildTree(right_preorder, right_inorder);
-//
-//        return root;
-//    }
-
-public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> inorder_index;
-        for(int i = 0; i < inorder.size(); i++)
-            inorder_index[inorder[i]] = i;
-        return recur(preorder, inorder_index, 0, 0, inorder.size() - 1);
+        int n = preorder.size();
+
+        for (int i = 0; i < n; ++i)
+            indexes[inorder[i]] = i;
+
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 
 private:
-    TreeNode* recur(vector<int>& preorder, unordered_map<int, int>& inorder_index, int root, int left, int right) {
-        if(right < left)
+    TreeNode* myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preHead, int preTail, int inoHead, int inoTail) {
+        if (preTail < preHead || inoTail < inoHead)
             return nullptr;
 
-        TreeNode* node = new TreeNode(preorder[root]);
-        int mid = inorder_index[preorder[root]];
-        node->left = recur(preorder, inorder_index, root + 1, left, mid - 1);
-        node->right = recur(preorder, inorder_index, root + mid - left + 1, mid + 1, right);
-        return node;
+        int value = preorder[preHead];
+
+        TreeNode* root = new TreeNode(value);
+
+        int leftLength = indexes[value] - inoHead;
+
+        int leftHead1 = preHead + 1;
+        int leftTail1 = preHead + leftLength;
+        int leftHead2 = inoHead;
+        int leftTail2 = indexes[value] - 1;
+
+        int rightHead1 = preHead + leftLength + 1;
+        int rightTail1 = preTail;
+        int rightHead2 = indexes[value] + 1;
+        int rightTail2 = inoTail;
+
+        root->left = myBuildTree(preorder, inorder, leftHead1, leftTail1, leftHead2, leftTail2);
+        root->right = myBuildTree(preorder, inorder, rightHead1, rightTail1, rightHead2, rightTail2);
+
+        return root;
     }
+
+private:
+    unordered_map<int, int> indexes;
 };
 
-void DLR(TreeNode* root) {
-    if (root == nullptr)
-        return;
-    cout << root->val << " ";
-    DLR(root->left);
-    DLR(root->right);
-}
-
 int main() {
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     Solution s;
     vector<int> preorder = {3,9,20,15,7};
     vector<int> inorder = {9,3,15,20,7};
     TreeNode* root = s.buildTree(preorder, inorder);
-    DLR(root);
+    showTree(root);
     cout << endl;
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
