c943b209dfd5ae001bb7703e21e32b0329101437
oshinoooo <1036858975@qq.com>
2022-04-09 16:23:48 -0400

    2022-04-09 16:23:47

--- offer0055-1/main.cpp
+++ offer0055-1/main.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
 };
 
-class Solution
-{
+class Solution {
 public:
-    int maxDepth(TreeNode* root)
-    {
-        if (!root)
-            return 0;
-        return max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
 
-int main()
-{
-    cout << "---------------------" << endl;
-    TreeNode* n1 = new TreeNode(1);
+int main() {
+    cout << "--------------------" << endl;
+    TreeNode* n1 = new TreeNode(4);
     TreeNode* n2 = new TreeNode(2);
-    TreeNode* n3 = new TreeNode(3);
-    TreeNode* n4 = new TreeNode(4);
-    TreeNode* n5 = new TreeNode(5);
-    TreeNode* n6 = new TreeNode(6);
+    TreeNode* n3 = new TreeNode(6);
+    TreeNode* n4 = new TreeNode(1);
+    TreeNode* n5 = new TreeNode(3);
+    TreeNode* n6 = new TreeNode(5);
     TreeNode* n7 = new TreeNode(7);
 
     n1->left = n2;
@@ -44,6 +38,6 @@ int main()
 
     Solution s;
     cout << s.maxDepth(n1) << endl;
-    cout << "---------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
