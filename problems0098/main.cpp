5706863bdbb800b0d99d214c0b5c1ccd3b642d41
oshinoooo <1036858975@qq.com>
2022-04-14 01:07:27 -0400

    2022-04-14 01:07:27

--- problems0098/main.cpp
+++ problems0098/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode* _left, TreeNode* _right) : val(_val), left(_left), right(_right) {}
 };
 
 class Solution {
@@ -64,8 +63,7 @@ private:
 };
 
 int main() {
-    cout << "-------------------" << endl;
-    Solution s;
+    cout << "--------------------" << endl;
     TreeNode* n1 = new TreeNode(4);
     TreeNode* n2 = new TreeNode(2);
     TreeNode* n3 = new TreeNode(6);
@@ -73,6 +71,7 @@ int main() {
     TreeNode* n5 = new TreeNode(3);
     TreeNode* n6 = new TreeNode(5);
     TreeNode* n7 = new TreeNode(7);
+
     n1->left = n2;
     n1->right = n3;
     n2->left = n4;
@@ -80,7 +79,8 @@ int main() {
     n3->left = n6;
     n3->right = n7;
 
+    Solution s;
     cout << s.isValidBST(n1) << endl;
-    cout << "-------------------" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
